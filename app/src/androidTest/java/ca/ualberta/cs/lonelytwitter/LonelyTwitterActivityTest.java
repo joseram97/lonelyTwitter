28f1c3fcf12e9e7f14dc17e06843d7ae1c03f8a5
Jose Ramirez <jose@ualberta.ca>
2018-10-24 18:13:10 -0600

    Added ui-testing changes

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
@@ -15,12 +15,38 @@ import junit.framework.TestCase;
  */
 public class LonelyTwitterActivityTest extends ActivityInstrumentationTestCase2<LonelyTwitterActivity> {
 
+    private Solo solo;
 
     public LonelyTwitterActivityTest() {
         super(ca.ualberta.cs.lonelytwitter.LonelyTwitterActivity.class);
     }
 
+    @Override
+    public void setUp() {
+        // this is for all the testing
+        // created a solo instance, and passed 2 parameters that are within the SOLO construct
+        solo = new Solo(getInstrumentation(), getActivity());
+    }
 
+    @Override
+    public void tearDown() {
+        solo.finishOpenedActivities();
+    }
 
+    // not all test methods that require testing MUST have the word "test" in the beginning
+    public void testTweet() {
+        solo.assertCurrentActivity("wrong activity", "LonelyTwitterActivity");
+    }
+
+    public void testEqual() {
+        assertEquals("not equal", "5", "6");
+    }
+
+    public void testAddTweet() {
+        solo.enterText((EditText) solo.getView(R.id.body), "new thing");
+        solo.clickOnButton("Save");
+        solo.clearEditText((EditText) solo.getView(R.id.body));
+        assertTrue(solo.waitForText("new thing"));
+    }
 
 }
